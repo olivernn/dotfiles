32550926b1ea249b0fa3f796c0b636e3508d7ecc
Oliver Nightingale <oliver.nightingale1@gmail.com>
2015-07-14 21:23:30 +0200

    Slight change to the zsh prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,3 @@
 PROMPT='%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg_bold[green]%}%~%{$reset_color%}
-%{$fg[white]%}✏ %{$reset_color%} '
+%{$fg[white]%}✎ %{$reset_color%} '
 RPROMPT='${vcs_info_msg_0_}%'
