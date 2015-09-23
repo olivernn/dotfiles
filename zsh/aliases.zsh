75de8d81d9f0742354a75e5d5f37727522ed8a09
Nightingale <oliven@685b357c7b09.ant.amazon.com>
2015-09-23 10:36:31 +0200

    OS X Linux compatibility fix

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,7 @@ alias tree='tree -C'
 
 alias dh='dirs -v'
 
-alias YYYY-MM-DD='date --utc +"%Y-%m-%d"'
-alias YYYY-MM-DD-HH='date --utc +"%Y-%m-%d-%H"'
+alias YYYY-MM-DD='date -u +"%Y-%m-%d"'
+alias YYYY-MM-DD-HH='date -u +"%Y-%m-%d-%H"'
 
 alias lines2spaces="awk -vORS=' ' '{ print $1 }'"
