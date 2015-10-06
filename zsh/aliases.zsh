bdc26369506531a31d6b0d962e256914413f2261
Oliver Nightingale <oliven@amazon.com>
2015-10-06 09:08:01 +0000

    Add alias to strip colour codes.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,3 +9,5 @@ alias YYYY-MM-DD='date -u +"%Y-%m-%d"'
 alias YYYY-MM-DD-HH='date -u +"%Y-%m-%d-%H"'
 
 alias lines2spaces="awk -vORS=' ' '{ print $1 }'"
+
+alias stripcolors='sed "s/\x1B\[\([0-9]\{1,2\}\(;[0-9]\{1,2\}\)\?\)\?[mGK]//g"'
