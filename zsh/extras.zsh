c7a186257bb7f95b2656be3bb4266402ffe3728b
Nightingale <oliven@a0999b15b629.ant.amazon.com>
2015-11-02 16:02:35 +0200

    Set localle correctly.

--- zsh/extras.zsh
+++ zsh/extras.zsh
@@ -1,3 +1,6 @@
 export DESKTOP="dev-dsk-oliven-1a-i-fc271d56.eu-west-1.amazon.com"
 alias desktop='ssh -A $DESKTOP'
 alias d='mosh $DESKTOP --ssh="ssh -o GSSAPITrustDns=no"'
+
+export LC_ALL="en_GB.UTF-8"
+export LANG="en_GB.UTF-8"
