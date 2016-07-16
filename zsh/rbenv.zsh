7cb72dec6c1335283e3438a13ecba578f2ecce59
Oliver Nightingale <oliver.nightingale1@gmail.com>
2016-07-16 12:05:52 +0000

    Only set up rbenv if rbenv is installed.

--- zsh/rbenv.zsh
+++ zsh/rbenv.zsh
@@ -1,3 +1,6 @@
 # rbenv
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+# Only want to do this if rbenv is installed
+if type "rbenv" > /dev/null; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+fi
