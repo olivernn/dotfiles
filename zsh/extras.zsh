6f8a1b6861e1a1cc4f223e163cfaf4661cfceafe
Oliver Nightingale <oliven@amazon.com>
2019-10-01 09:29:35 +0200

    Add shortcut for UBUNTU machine

--- zsh/extras.zsh
+++ zsh/extras.zsh
@@ -1,6 +1,9 @@
 export DESKTOP="dev-dsk-oliven-1b-517af0f2.eu-west-1.amazon.com"
 alias desktop='ssh -A $DESKTOP'
 alias d='mosh $DESKTOP'
+
+export UBUNTU="ub3d091c6ac945a.ant.amazon.com"
+
 alias mssh="/usr/local/bin/mssh -A"
 
 export LC_ALL="en_GB.UTF-8"
