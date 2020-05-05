fb7406733019d21cbdb106f8a520e2906a14fef8
Oliver Nightingale <oliven@amazon.com>
2020-05-05 09:43:14 +0200

    Add workaround for ruby and openssl

--- zsh/rbenv.zsh
+++ zsh/rbenv.zsh
@@ -4,3 +4,8 @@ if type "rbenv" > /dev/null; then
   export PATH="$HOME/.rbenv/bin:$PATH"
   eval "$(rbenv init -)"
 fi
+
+# from the ruby-build post install message
+if type "brew" > /dev/null; then
+  export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+fi
