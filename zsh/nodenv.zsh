d35c4f129fd49bcd7de890bd8208c66b87a705e3
Oliver Nightingale <oliven@amazon.com>
2022-06-06 12:15:51 +0100

    Update nodeenv
    Yes, I know this is technically a different tool, but whatever

--- zsh/nodenv.zsh
+++ zsh/nodenv.zsh
@@ -1,3 +1,3 @@
-if type "nodenv" > /dev/null; then
-  eval "$(nodenv init -)"
-fi
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
