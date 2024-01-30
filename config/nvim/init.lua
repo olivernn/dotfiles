686def96ac401f9403c801426b11d8ea6ca255cf
Oliver Nightingale <oliven@dev-dsk-oliven-1b-a404e8fc.eu-west-1.amazon.com>
2024-01-30 12:01:25 +0000

    Update nvim configuration

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -44,9 +44,14 @@ local opts = {
   }
 }
 
+vim.g.aws_profile = "oliven"
+
 require('lazy').setup({
   { 'williamboman/mason.nvim' },
   { 'williamboman/mason-lspconfig.nvim' },
+  { 'mfussenegger/nvim-jdtls',
+    lazy = true
+  },
   {
     'maxmx03/dracula.nvim',
     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
@@ -108,8 +113,27 @@ require('lazy').setup({
         }
       })
     end
-  }
+  },
 }, opts)
+
+function bemol()
+  local bemol_dir = vim.fs.find({ '.bemol' }, { upward = true, type = 'directory' })[1]
+  local ws_folders_lsp = {}
+
+  if bemol_dir then
+    local file = io.open(bemol_dir .. '/ws_root_folders', 'r')
+    if file then
+      for line in file:lines() do
+        table.insert(ws_folders_lsp, line)
+      end
+      file:close()
+    end
+  end
+
+  for _, line in ipairs(ws_folders_lsp) do
+    vim.lsp.buf.add_workspace_folder(line)
+  end
+end
 
 local lsp_zero = require('lsp-zero')
 
@@ -121,6 +145,8 @@ lsp_zero.on_attach(function(_, bufnr)
   vim.keymap.set('n', '<LEADER>r', '<cmd>lua vim.lsp.buf.rename()<CR>', { buffer = bufnr })
   vim.keymap.set('n', '<LEADER>f', '<cmd>lua vim.lsp.buf.format()<CR>', { buffer = bufnr })
   vim.keymap.set('n', '<LEADER>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', { buffer = bufnr })
+
+  bemol()
 end)
 
 lsp_zero.set_sign_icons({
@@ -143,6 +169,9 @@ local cmp_action = require('lsp-zero').cmp_action()
 cmp.setup({
   confirmation = {
     completeopt = 'menu,menuone,noinsert'
+  },
+  experimental = {
+    ghost_text = true
   },
   preselect = 'item',
   mapping = cmp.mapping.preset.insert({
@@ -170,6 +199,7 @@ require('lualine').setup()
 
 vim.cmd.colorscheme 'dracula'
 
+vim.g.netrw_browsex_viewer = 'local-open'
 vim.g.mapleader = ","
 
 vim.keymap.set('n', '<C-J>', '<C-W><C-J>')
